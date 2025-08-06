43d7ddfa7202eac8c8325153354a4ddcc7feef10
Subhana <subhanasn2005@gmail.com>
2025-08-06 14:45:13 +0530

    unneccessary files removed

--- frontend/src/types/tokenTypes.ts
+++
@@ -1,6 +0,0 @@
-export interface JwtPayloadWithRole {
-  id: string;
-  role: 'user' | 'mentor' | 'admin'; // adjust if you have different roles
-  iat: number; // issued at
-  exp: number; // expiration time
-}
