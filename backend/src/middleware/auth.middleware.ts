b1914c74701235374d7f08e973197fdcd9bebf63
Subhana <subhanasn2005@gmail.com>
2025-11-19 21:55:06 +0530

    changes in middleware

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { Response, NextFunction, RequestHandler } from "express";
-import { MESSAGES, STATUS_CODES } from "../utilis/constants";
+import { MESSAGES, STATUS_CODES,ROLES,COOKIE_KEYS } from "../utilis/constants";
 import { AuthenticatedRequest } from "../types/authenticatedRequest";
 import dotenv from "dotenv";
 dotenv.config();
@@ -8,7 +8,7 @@ dotenv.config();
 export const authMiddleware = (allowedRoles: string[]): RequestHandler => {
   return (req, res, next) => {
     const token =
-      req.cookies["auth-token"] || req.headers.authorization?.split(" ")[1];
+      req.cookies[COOKIE_KEYS.AUTH] || req.headers.authorization?.split(" ")[1];
 
     if (!token) {
       res.status(STATUS_CODES.UNAUTHORIZED).json({
@@ -20,7 +20,7 @@ export const authMiddleware = (allowedRoles: string[]): RequestHandler => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
         id: string;
-        role: "user" | "mentor" | "admin";
+        role: (typeof ROLES)[keyof typeof ROLES];
       };
 
       if (!allowedRoles.includes(decoded.role)) {
