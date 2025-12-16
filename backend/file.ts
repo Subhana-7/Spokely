f1cf8fdf064c090244aecd36ae6b3ff2e8be344a
Subhana <subhanasn2005@gmail.com>
2025-12-16 21:36:19 +0530

    changes

--- backend/file.ts
+++
@@ -1,7 +0,0 @@
-import { chatWithGroq } from "./src/config/groq.client";
-
-async function run() {
-  const feedback = await chatWithGroq("Give feedback on: My favorite food is pizza.");
-}
-
-run();
