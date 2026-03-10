f0391e2ff8b053339b6888a15e940d45ada18b0b
Subhana <subhanasn2005@gmail.com>
2026-03-10 12:50:30 +0530

    nodemailer render issue

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -16,8 +16,8 @@ export class EmailService implements IEmailService {
   constructor() {
     this._transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
@@ -92,8 +92,8 @@ export class EmailService implements IEmailService {
       console.log("hit here");
       const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
